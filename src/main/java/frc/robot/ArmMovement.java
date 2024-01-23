78460b6ddc9688ea916e1b01177a9c03f4d6f96b
Logan Knott <loganeknott@gmail.com>
2024-01-22 20:41:58 -0500

    Added RPM to velocity conversion(untested)
    Quickly added RPM to velocity conversion formula but it is untested.

--- src/main/java/frc/robot/ArmMovement.java
+++ src/main/java/frc/robot/ArmMovement.java
@@ -1,6 +1,7 @@
 package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 
 public class ArmMovement {
     
@@ -8,14 +9,18 @@ public class ArmMovement {
     WPI_VictorSPX m_intake, m_shoot, m_armOne, m_armTwo; //motor object 
     double m_speed = 0.8;  //-1.0 to 1.0    motor speed 
     double a_speed = 0.2; //-1.0 to 1.0     arm speed
- 
+    
+    double shootRPM;
+    double intakeRPM;
 
 
     //Todo figure out encoders with SPX 
     //There has to be a better way to make these motors move right??? This is so repetitive 
     public void intake(){ 
         m_intake = new WPI_VictorSPX(3); 
-        m_intake.set(m_speed);
+        
+        double m_velocity = 2048 / intakeRPM / 600;
+        m_intake.set(ControlMode.Velocity, m_velocity);
 
         //intake till prox sensor says stop 
     }   
@@ -23,7 +28,9 @@ public class ArmMovement {
 
     public void shoot(){ 
         m_shoot = new WPI_VictorSPX(4); 
-        m_shoot.set(m_speed);
+        
+        double m_velocity = 2048 / shootRPM / 600;
+        m_shoot.set(ControlMode.Velocity, m_velocity);
 
         //shoot till prox sensor says stop 
 
