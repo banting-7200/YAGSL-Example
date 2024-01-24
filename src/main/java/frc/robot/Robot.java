4041052a9329f762a33afd779070b8575f14e840
jsrii <jas.kanthan@gmail.com>
2024-01-24 15:53:39 -0500

    More debugging

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -46,7 +46,8 @@ public class Robot extends TimedRobot
   public void robotInit()
   {
     PathPlannerServer.startServer(5811);
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    System.out.println("This is so slaytastic");
+        // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
 
